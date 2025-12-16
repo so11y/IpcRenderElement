f225ecadc92cbe9d903b4dd0d7d385f44098a71d
Sorry <5646769+sorry1@user.noreply.gitee.com>
2025-12-16 14:04:08 +0800

    feat: 1

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import test from "./p/index";
-import { NodeTypes, ElementTypes } from "@vue/compiler-core";
-// import { templateCompilerOptions } from "@tresjs/core";
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx()
-    // test()
-  ],
+  plugins: [vue(), vueJsx()],
   server: {
     headers: {
-      // 允许所有域名跨域访问
       "Access-Control-Allow-Origin": "*"
     }
   }
